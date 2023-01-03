fa2f43c34b2d3402d853d38b210b2b6114285502
Yuri Voinov <yvoinov@gmail.com>
2023-01-03 20:24:49 +0600

    Fixup typo MPROVEMENT: More refactoring - the inner class is defined outside the body of the main class.

--- src/memlogger.h
+++ src/memlogger.h
@@ -111,35 +111,7 @@ class MemoryLoggerFunctions {
 
 		~MemoryLoggerFunctions() { printReportOnExit(); }
 	private:
-		class AdaptiveSpinMutex {
-			public:
-				AdaptiveSpinMutex(std::atomic<bool>& v_lock) : m_lock(v_lock) {};
-				AdaptiveSpinMutex(const AdaptiveSpinMutex&) = delete;
-				~AdaptiveSpinMutex() = default;
-
-				void lock() noexcept {
-					T v_spin_count { 0 };
-
-					while (m_lock.load(std::memory_order_relaxed) || m_lock.exchange(true, std::memory_order_acquire)) {
-						++v_spin_count;
-						if (v_spin_count < m_spin_pred << 1) continue;	/* m_spin_pred << 1 is eq m_spin_pred * 2 */
-						std::unique_lock<std::mutex> tlock(m_conditional_mutex);
-						m_conditional_lock.wait_for(tlock, std::chrono::nanoseconds(1), [this]() { return !m_lock.load(std::memory_order_relaxed); });
-					}
-
-					m_spin_pred += (v_spin_count - m_spin_pred) >> 3;	/* x >> 3 is eq x / 8 */
-				}
-
-				void unlock() noexcept {
-					m_lock.store(false, std::memory_order_release);
-					m_conditional_lock.notify_one();
-				}
-			private:
-				std::atomic<bool>& m_lock;
-				std::atomic<T> m_spin_pred { 0 };
-				std::mutex m_conditional_mutex;
-				std::condition_variable m_conditional_lock;
-		};
+		class AdaptiveSpinMutex;
 
 		MemoryLoggerFunctions() : m_fname(std::getenv("MEMLOGGER_LOG_FILENAME")) {
 			std::signal(SIGINT, signal_handler);
