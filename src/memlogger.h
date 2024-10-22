559aafc2cd1868d0493d2421792a803a3ead7659
Yuri Voinov <yvoinov@gmail.com>
2024-10-22 17:04:07 +0500

    Update memlogger.h

--- src/memlogger.h
+++ src/memlogger.h
@@ -257,24 +257,6 @@ private:
 		L allc_max {};					/* Max allocation size */
 		std::time_t start {}, stop {};			/* Time interval */
 		Fl lock { MEMLOGGER_FLAG_DEFAULT };
-
-		Counters& operator=(const Counters& other) {	/* Will use custom copy assignment overload */
-			if (this != &other) {
-				allc_64k = other.allc_64k;
-				allc_128k = other.allc_128k;
-				allc_256k = other.allc_256k;
-				allc_512k = other.allc_512k;
-				allc_1024k = other.allc_1024k;
-				allc_2048k = other.allc_2048k;
-				allc_4096k = other.allc_4096k;
-				allc_8192k = other.allc_8192k;
-				allc_more = other.allc_more;
-				allc_max = other.allc_max;
-				start = other.start;
-				stop = other.stop;
-			}
-			return *this;
-		}
 	};
 
 	std::array<Counters, m_c_array_size> m_CounterArray;
