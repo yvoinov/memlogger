bb717e381e22f5ed521183fde1d73cb33447dd78
Yuri Voinov <yvoinov@gmail.com>
2025-11-30 21:07:44 +0500

    Added check interference sizes.
    Added check interference sizes to align counters structures to avoid false sharing.

---
+++ cmake_check_interference_sizes.cmake
@@ -0,0 +1,32 @@
+# --- Test interference sizes ---
+file(WRITE "${CMAKE_BINARY_DIR}/check_interference_sizes.cpp" "
+#include <new>
+int main() {
+#ifdef __cpp_lib_hardware_interference_size
+    return !(std::hardware_constructive_interference_size &&
+             std::hardware_destructive_interference_size);
+#else
+    return 1;
+#endif
+}
+")
+
+try_run(
+    INTERFERENCE_RUN_RESULT
+    INTERFERENCE_COMPILE_RESULT
+    ${CMAKE_BINARY_DIR}
+    ${CMAKE_BINARY_DIR}/check_interference_sizes.cpp
+)
+
+if (INTERFERENCE_COMPILE_RESULT AND INTERFERENCE_RUN_RESULT EQUAL 0)
+    message(STATUS "STL provides hardware interference sizes")
+    add_compile_definitions(INTERFERENCE_SIZES)
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        check_cxx_compiler_flag("-Wno-interference-size" HAVE_NO_INTERFERENCE_WARN)
+        if (HAVE_NO_INTERFERENCE_WARN)
+            add_compile_options(-Wno-interference-size)
+        endif()
+    endif()
+else()
+    message(STATUS "No interference sizes in STL")
+endif()
